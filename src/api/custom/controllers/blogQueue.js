1e759797dcb9f6cd6b90870975a51f07c2247fa3
Vijendra Kumar Saini <vijendrasaini@startbitsolutions.com>
2025-09-01 18:37:02 +0530

    updadted connection info
    updadted connection info

--- src/api/custom/controllers/blogQueue.js
+++ src/api/custom/controllers/blogQueue.js
@@ -1,7 +1,7 @@
 const Queue = require('bull');
 const Redis = require('ioredis');
 
-// Initialize Redis connection
+// Initialize Redis connection info
 const redis = new Redis();
 
 // Create a queue
