e9d6711a9be06381f917c69df5909d36a64566c5
Vijendra Kumar Saini <vijendrasaini@startbitsolutions.com>
2025-09-15 11:53:52 +0530

    Updating query result
    Updating query result

--- src/api/custom/controllers/blogQueue.js
+++ src/api/custom/controllers/blogQueue.js
@@ -23,7 +23,7 @@ blogQueue.process(async (job) => {
       limit: 1,        
     });
     console.log(`Searching for blog with title: '${Title.trim()}'`);
-    console.log('Query result:', existingBlog);
+    console.log('Query result pp:', existingBlog);
     if (existingBlog.length > 0) {
       console.log('Blog with this title already exists');
     } else {
