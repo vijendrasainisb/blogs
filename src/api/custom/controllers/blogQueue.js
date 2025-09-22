e605ccbe2e5cbc2ae9964b7b778a1989c20b708b
Vijendra Kumar Saini <vijendrasaini@startbitsolutions.com>
2025-09-22 09:48:27 +0530

    updated process output

--- src/api/custom/controllers/blogQueue.js
+++ src/api/custom/controllers/blogQueue.js
@@ -12,7 +12,7 @@ const blogQueue = new Queue('blogQueue', {
   },
 });
 
-// Define a job processor for process
+// Define a job processor for process output
 blogQueue.process(async (job) => {
   const { Title, Description, Image, Status } = job.data;
 
