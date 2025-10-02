a35d916fa0669e62fee738ac5da180b0aff0b89c
Vijendra Kumar Saini <vijendrasaini@startbitsolutions.com>
2025-10-02 10:23:02 +0530

    undo strapi changes

--- src/api/custom/controllers/custom.js
+++ src/api/custom/controllers/custom.js
@@ -30,7 +30,7 @@ module.exports = {
       }
 
         ctx.body = {
-          message: 'Horray! New blog created successfully in strapi'
+          message: 'Horray! New blog created successfully'
         };
       
     } catch (err) {
