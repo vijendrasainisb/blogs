70e987a5b87470efd9e2a755993a5fbcb42d4e83
Vijendra Kumar Saini <vijendrasaini@startbitsolutions.com>
2025-10-03 10:05:51 +0530

    updated queue process

--- src/api/custom/controllers/custom.js
+++ src/api/custom/controllers/custom.js
@@ -17,7 +17,7 @@ module.exports = {
 
       const { Title, Description, Image, Status } = Body;
 
-        // Loop to add 500 posts to the queue
+        // Loop to add 500 posts to the queue row
       for (let i = 1; i <= 5000; i++) {
         const postTitle = `${Title}${i}`;
         
@@ -30,7 +30,7 @@ module.exports = {
       }
 
         ctx.body = {
-          message: 'Horray! New blog created successfully'
+          message: 'Horray! New blog created successfully.'
         };
       
     } catch (err) {
